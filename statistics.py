6032e5c42497bf89a64e1fd4296b391a03041cf7
lacmih <72100848+lacmih@users.noreply.github.com>
2023-11-24 00:03:07 +0200

    Add files via upload

---
+++ statistics.py
@@ -0,0 +1,174 @@
+participant_programs_sorted = {
+    'A. Dávid': ['Fotómaraton', 'Táblé', 'City Tour', 'Sakk'],
+    'A. Eszter': ['Néptánc', 'City Tour', 'Női foci'],
+    'B. Előd': ['Póker', 'PC LOL', 'PC:CS-GO'],
+    'Bálint Dániel': ['Férfi Foci'],
+    'Bálint Kund': ['Cooltúra'],
+    'Bereczki-Orbán András': ['Méta', 'Kézilabda', 'Férfi Foci', 'Kultúrmaraton'],
+    'Bogi': ['City Tour', 'Chest-Pong'],
+    'Bodrogi': ['Biliárd', 'Méta', 'Kosárlabda', 'Jenga', 'Férfi Foci'],
+    'Csató Hunor': ['Klán-Tour', 'City Tour'],
+    'Csanád': ['Kézilabda', 'Méta'],
+    'Francis': ['Klán-Tour', 'Női foci'],
+    'Gabi': ['Fotómaraton', 'City Tour'],
+    'Ilyés Gyopár': ['Kultúrmaraton', 'Erotik'],
+    'Iszlai Virág': ['Kosárlabda', 'Női foci'],
+    'Jenei Bence': ["Méta"],
+    'Kiki': ['Méta', 'Csocsó', 'Karaoke', 'Kosárlabda', 'Női foci'],
+    'Kinga': ['Kézilabda', 'Klán-Tour', 'City Tour', 'Női foci'],
+    'Kupán': ['Bolc', 'Chest-Pong', 'City Tour', 'Erotik'],
+    'Laci': ['Méta', 'Csocsó', 'Karaoke'],
+    'M. Dávid': ['Méta', 'City Tour', 'Úszás', 'Kultúrmaraton', 'Klán-Tour'],
+    'Nyisztor': ['Méta', 'Kézilabda', 'Férfi Foci', 'Röplabda', 'Kosárlabda', 'Erotik'],
+    'OP': ['Méta', 'Férfi Foci', 'Erotik'],
+    'Olasz Bence': [],
+    'P.Péter': ['Asztalitenisz', 'Táncmaraton', 'Férfi Foci'],
+    'Petra': ['Staféta', 'Kézilabda', 'Méta', 'Úszás', 'Kosárlabda', 'Dance Battle', 'Slam Poetry', 'Klán-Tour Night', 'Erotik'],
+    'S. Előd': ['Méta', 'Néptánc'],
+    'Sarah': ['Fotómaraton'],
+    'Sebesi Máté': [],
+    'Simon Janka': ['Klán-Tour'],
+    'Sz. Csongi': ['Méta', 'Biliárd', 'Férfi Foci', 'PC:CS-GO', 'Sakk'],
+    'Szász Andrea': ['Fotómaraton', 'Klán-Tour', 'Táncmaraton'],
+    'Toth Bea': ['Fotómaraton', 'Méta', 'Kosárlabda', 'Női foci', 'Asztalitenisz'],
+    'Tunyogi Kata': ['Méta', 'Kézilabda', 'Röplabda', 'Női foci', 'City Tour', 'Kosárlabda'],
+    'Vitus Szabolcs': ['Staféta', 'Klán-Tour', 'Beer-Pong', 'Klán-Tour Night'],
+    'Zsófi': ['Méta', 'Erotik']
+}
+
+program_points = {
+    'Asztalitenisz': 25,
+    'Beer-Pong': 50,
+    'Bemutatkozó': 208,
+    'Biliárd': 20,
+    'Bogrács': 0,
+    'Bolc': 140,
+    'Chandelier': 0,
+    'Chest-Pong': 50,
+    'City Tour': 170,
+    'Cooltúra': 85,
+    'PC:CS-GO': 50,
+    'Csocsó': 140,
+    'Dance Battle': 0,
+    'Erotik': 213,
+    'Férfi Foci': 50,
+    'Fotómaraton': 110,
+    'Jenga': 150,
+    'Karaoke': 240,
+    'Kézilabda': 50,
+    'Klán-Tour': 150,
+    'Klán-Tour Night': 155,
+    'Kosárlabda': 90,
+    'Kultúrmaraton': 174,
+    'PC LOL': 50,
+    'Méta': 130,
+    'Néptánc': 86,
+    'Női foci': 90,
+    'Póker': 55,
+    'Röplabda': 70,
+    'Sakk': 40,
+    'Slam Poetry': 50,
+    'Staféta': 86,
+    'Táblé': 30,
+    'Táncmaraton': 345,
+    'Úszás': 82,
+    'Össz': 3434
+}
+
+program_participants = {
+    'Méta': ['M. Laci', 'Kiki', 'Bodrogi', 'Nyisztor', 'T. Kata', 'Sz. Csongi', 'S. Előd', 'Petra', 'OP', 'BOA', 'M. Dávid', 'Csani', 'Zsófi', "Jenei Bence"],
+    'Staféta': ['Petra', 'V.Szabi'],
+    'Táblé': ['A. Dávid'],
+    'Chest-Pong': ['M. Bogi', 'Kupán'],
+    'Póker': ['B. Előd'],
+    'Kézilabda': ['Petra', 'T.Kata', 'Nyisztor', 'Bodrogi', 'Cs.Csanád', 'Kinga', 'BOA'],
+    'Bolc': ['Kupán'],
+    'Biliárd': ['Bodrogi', 'Sz. Csongi'],
+    'Néptánc': ['A. Eszter', 'Előd'],
+    'Klán-Tour': ['V.Szabi', 'Sz. Andi', 'Janka', 'Marosi Dàvid', 'Kinga', 'Francis', 'Huni'],
+    'Dance Battle': ['Petra', 'asd', 'asd', 'asd', 'asd', 'asd'],
+    'Karaoke': ['M. Laci', 'Kiki'],
+    'City Tour': ['Andi', 'Gabi', 'Huni', 'Kata', 'M. Dávid', 'Kupán', 'Bogi', 'Eszter', 'A. Dávid', 'Kinga'],
+    'Úszás': ['Petra', 'M. Dávid'],
+    'PC:CS-GO': ['Sz. Csongi', 'Előd', 'asd', 'asd', 'asd'],
+    'Táncmaraton': ['Sz. Andi', 'P. Péter'],
+    'Chandelier': [],
+    'Csocsó': ['M. Laci', 'Kiki'],
+    'Asztalitenisz': ['P.Péter', 'Bea'],
+    'Kosárlabda': ['Nyisztor', 'Bodrogi', 'T. Kata', 'Petra', 'Kiki', 'BOA', 'Virág', 'Bea'],
+    'Cooltúra': ['Kund'],
+    'Jenga': ['Bodrogi'],
+    'Slam Poetry': ['Petra', 'asd', 'asd'],
+    'Férfi Foci': ['Sz. Csongi', 'OP', 'P.Péter', 'Bodrogi', 'BOA', 'Nyisztor', 'Dani'],
+    'PC LOL': ['B. Előd', 'asd', 'asd', 'asd', 'asd'],
+    'Röplabda': ['Nyisztor', 'T.Kata', 'asd', 'asd', 'asd', 'asd'],
+    'Kultúrmaraton': ['I. Gyopi', 'BOA', 'M. Dávid'],
+    'Beer-Pong': ['Szabi'],
+    'Sakk': ['A. Dávid', 'Sz. Csongi'],
+    'Női foci': ['T. Kata', 'Kiki', 'Virág', 'Bea', 'Kinga', 'Francis', 'Eszter'],
+    'Fotómaraton': ['A. Dávid',	'Bea', 'Sz. Andi', 'Gabi', 'Sarah'],
+    'Klán-Tour Night': ['Petra', 'Vitus Szablolcs'],
+    'Erotik': ['Kupán', 'Op', 'Nyisztor', 'Zsófi', 'Petra', 'Gyopi'],
+}
+
+introduction_missers = ['Bálint Dániel', 'Iszlai Virág']
+introduction_participants = []
+for participant, programs in participant_programs_sorted.items():
+    if participant not in programs:
+        participant_programs_sorted[participant].append('Bemutatkozó')
+        introduction_participants.append(participant)
+
+program_participants['Bemutatkozó'] = introduction_participants
+
+participants = list(participant_programs_sorted.keys())
+
+seen_programs = []
+
+matrix = []
+for person in participants:
+    row = []
+    for program in program_points:
+        if program in participant_programs_sorted[person]:
+            participation_count = len(program_participants[program])
+            points = program_points[program] / participation_count
+            # print(person, program, points)
+            if program not in seen_programs:
+                seen_programs.append(program)
+
+            row.append(round(points, 2))
+        else:
+            row.append(0)
+    matrix.append(row)
+
+print("Programok, amiket nem veszünk bele, vagy nem voltunk:")
+for i in list(program_points.keys()):
+    if i not in seen_programs:
+        print(i)
+
+print("Ember-Program mátrix:")
+print("#########################")
+for i in range(len(participants)):
+    print(participants[i], matrix[i])
+
+participant_points = [sum(row) for row in matrix]
+
+zipped_data = list(zip(participants, participant_points))
+
+sorted_data = sorted(zipped_data, key=lambda x: x[1])
+
+sorted_names = [data[0] for data in sorted_data]
+sorted_row_sums = [data[1] for data in sorted_data]
+
+# for i in sorted_names:
+#     print(i)
+# for i in sorted_row_sums:
+#     print(i)
+
+# for i in sorted_names:
+#     print(len(participant_programs_sorted[i]))
+# print("Pontszámok sorrendben:", sorted_row_sums)
+# print("Nevek sorrendben:", sorted_names)
+
+for i in range(len(sorted_row_sums)):
+    print(str(sorted_names[i]) + ": " + str(sorted_row_sums[i]))
+
